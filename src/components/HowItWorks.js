3471f6ba5ee912570442c4f777405a4a62353830
Carlos Martinez <martinezcj2@gmail.com>
2018-12-18 03:54:21 -0500

    Build out HowItWorks component

---
+++ src/components/HowItWorks.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const HowItWorks = () => {
+  return (
+    <section className="section-howitworks">
+      <div className="how-it-works">
+        <h2 className="how-it-works__title">how dreamshare works</h2>
+        <div className="how-it-works__cards">
+          <article className="how-it-works__card how-it-works__card--1">
+            <div className="how-it-works__content">
+              <p className="how-it-works__step">step 1</p>
+              <h3 className="how-it-works__sub-title">Sed leo enim, condimentum</h3>
+              <p className="how-it-works__para">Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis.</p>
+            </div>
+          </article>
+          <article className="how-it-works__card how-it-works__card--2">
+            <div className="how-it-works__content">
+              <p className="how-it-works__step">step 2</p>
+              <h3 className="how-it-works__sub-title">Sed leo enim, condimentum</h3>
+              <p className="how-it-works__para">Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis.</p>
+            </div>
+          </article>
+          <article className="how-it-works__card how-it-works__card--3">
+            <div className="how-it-works__content">
+              <p className="how-it-works__step">step 3</p>
+              <h3 className="how-it-works__sub-title">Sed leo enim, condimentum</h3>
+              <p className="how-it-works__para">Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis.</p>
+            </div></article>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HowItWorks;
