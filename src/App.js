d2c92c5a54e99d06a39493ab42c74df3642be7c4
Carlos Martinez <martinezcj2@gmail.com>
2018-12-18 01:52:00 -0500

    Import Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import './css/style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        this is the main app.
+        <Header />
       </div>
     );
   }
