a4430603f2b624fc944fc051194a5b39d2407407
Carlos Martinez <martinezcj2@gmail.com>
2018-12-18 15:16:48 -0500

    Build & add data to HowItWorks

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -1,31 +1,43 @@
 import React from "react";
 
 const HowItWorks = () => {
+  const cards = [
+    {
+      id: '1',
+      title: "Sed leo enim, condimentum",
+      para: "Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis."
+    },
+    {
+      id: '2',
+      title: "Morbi velit risus",
+      para: "Nulla venenatis tempor dui in molestie. Nulla quis dictum purus, sit amet porttitor est."
+    },
+    {
+      id: '3',
+      title: "Sed leo enim, condimentum",
+      para: "Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis."
+    }
+  ]
+
   return (
     <section className="section-howitworks">
       <div className="how-it-works">
         <h2 className="how-it-works__title">how dreamshare works</h2>
         <div className="how-it-works__cards">
-          <article className="how-it-works__card how-it-works__card--1">
-            <div className="how-it-works__content">
-              <p className="how-it-works__step">step 1</p>
-              <h3 className="how-it-works__sub-title">Sed leo enim, condimentum</h3>
-              <p className="how-it-works__para">Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis.</p>
-            </div>
-          </article>
-          <article className="how-it-works__card how-it-works__card--2">
-            <div className="how-it-works__content">
-              <p className="how-it-works__step">step 2</p>
-              <h3 className="how-it-works__sub-title">Sed leo enim, condimentum</h3>
-              <p className="how-it-works__para">Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis.</p>
-            </div>
-          </article>
-          <article className="how-it-works__card how-it-works__card--3">
-            <div className="how-it-works__content">
-              <p className="how-it-works__step">step 3</p>
-              <h3 className="how-it-works__sub-title">Sed leo enim, condimentum</h3>
-              <p className="how-it-works__para">Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis.</p>
-            </div></article>
+          {
+            cards.map((card, key) => {
+              let { id, title, para } = card;
+              return (
+                <article key={key} className={ `how-it-works__card how-it-works__card--${card.id}` }>
+                  <div className="how-it-works__content">
+                    <p className="how-it-works__step">{ `step ${ id }` }</p>
+                    <h3 className="how-it-works__sub-title">{ title }</h3>
+                    <p className="how-it-works__para">{ para }</p>
+                  </div>
+                </article>
+              )
+            })
+          }
         </div>
       </div>
     </section>
